62ec7a56c2fdde605994bc77db105212e8d07052
Lanzhijiang <lanzhijiang@foxmail.com>
2025-12-29 16:05:37 +0800

    update migration

--- server/database/migrations/0004_add_work_item_upload_fields.sql
+++
@@ -1,4 +0,0 @@
--- Add upload step result fields to work_items table
-ALTER TABLE work_items ADD COLUMN dataset_id INTEGER;
-ALTER TABLE work_items ADD COLUMN feature_columns TEXT;
-ALTER TABLE work_items ADD COLUMN target_column TEXT;
