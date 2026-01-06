7fa8c9f3c93253228b3a7e6bcc0639b43fb7ac21
Lanzhijiang <lanzhijiang@foxmail.com>
2026-01-06 11:36:10 +0800

    Revert "Fix TypeScript build errors: remove duplicate exports, fix routes, and correct component types"
    This reverts commit 94e941974ebee5a53f6a07da842ea50c68d35c48.

--- packages/frontend/src/composables/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Composables Index
- * Export all composables from one place
- */
-export * from './useProjects';
-export * from './useWorkItems';
-export * from './useDatasets';
-export * from './useTasks';
-export * from './useFormatters';
